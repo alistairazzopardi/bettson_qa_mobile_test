97b999ec51ebbd6497bd526c6ba52c6f79be5347
pingponggamesstudios <75321815+pingponggamesstudios@users.noreply.github.com>
2025-09-24 21:57:49 +0200

    added driver manager

---
+++ src/Driver/DriverManager.java
@@ -0,0 +1,27 @@
+package Driver;
+
+import java.net.MalformedURLException;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class DriverManager {
+    public static AndroidDriver driver;
+
+    public static void initDriver() throws MalformedURLException {
+        UiAutomator2Options options = new UiAutomator2Options()
+                .setPlatformName("Android")
+                .setDeviceName("dead23b55c8d")
+                .setApp("C:\\Users\\Alist\\CrossDevice\\POCO X4 Pro 5G\\storage\\Download");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+    }
+
+    public static void quitDriver() {
+        if (driver != null) driver.quit();
+    }
+}
